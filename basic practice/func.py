324c27a833a27132b3f3c3e7916e43e6e67486e8
mateegojra <thedonofgojra@gmail.com>
2018-09-27 20:45:38 +0500

    Basic Table using while loop and defitions of python

--- basic practice/func.py
+++ basic practice/func.py
@@ -1,11 +1,6 @@
-def ask_ok(prompt, retries=4, reminder='Please try again!'):
-    while True:
-        ok = input(prompt)
-        if ok in ('y', 'ye', 'yes'):
-            return True
-        if ok in ('n', 'no', 'nop', 'nope'):
-            return False
-        retries = retries - 1
-        if retries < 0:
-            raise ValueError('invalid user response')
-        print(reminder)
+def drawTable(t):
+    i = 0
+    while i<=10:
+        print(str(t)+" X "+str(i)+" = "+str(i*t))
+        i = i+1
+drawTable(5)
